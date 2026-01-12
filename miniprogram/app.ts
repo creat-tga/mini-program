0a717ebe190461710a3c89d40d568b3ce254838a
creat-tga <1065983258@qq.com>
2026-01-12 20:15:37 +0800

    0112v1

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -48,7 +48,8 @@ App<IAppOption>({
     wx.loadFontFace({
       global: true,  // 全局生效
       family: 'SourceHanSansSC',
-      source: `url("https://1065983258.dpdns.org:8000/static/SimplifiedChinese/STXINGKA.TTF")`,
+      // source: `url("http://localhost:8000/static/SimplifiedChinese/STXINGKA.TTF")`,
+      source: `url("https://1065983258.dpdns.org/static/STXINGKA.TTF")`, 
       // source: `url("https://lf3-static.bytednsdoc.com/obj/eden-cn/89eh7p9v_zlp/ljhwZthlaukjlkulzlp/fonts/SourceHanSansSC-Regular.woff2")`,
       success(res) {
         console.log(`SourceHanSansSC-VF.ttf加载成功`, res);
